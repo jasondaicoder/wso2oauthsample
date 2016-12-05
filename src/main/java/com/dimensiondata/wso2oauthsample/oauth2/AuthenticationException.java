b965f4641ac0c45b719457f133ecd029fd5f5d45
jason.dai <jasondai.inbox@gmail.com>
2016-12-05 15:26:39 +1100

    refactorred the code to use pure jdk and apache libs instead of wso2 libs

---
+++ src/main/java/com/dimensiondata/wso2oauthsample/oauth2/AuthenticationException.java
@@ -0,0 +1,62 @@
+/**
+ * AuthenticationException.java
+ *
+ * @author jason
+ * @version 1.0
+ * @since 5Dec.,2016
+ */
+package com.dimensiondata.wso2oauthsample.oauth2;
+
+/**
+ *
+ * @author jason
+ *
+ */
+public class AuthenticationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	public AuthenticationException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public AuthenticationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public AuthenticationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public AuthenticationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public AuthenticationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
